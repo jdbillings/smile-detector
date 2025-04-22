4e3432f5b4e26104548601ccc127cc17a87525d5
Josh Billings <jdb1025777@gmail.com>
2025-04-21 21:00:42 -0400

    refactor: frame generator will check db for liveliness every few tries

--- python/smile_detector/session_manager.py
+++ python/smile_detector/session_manager.py
@@ -5,46 +5,55 @@ from typing import Tuple, Self
 from cv2.typing import MatLike
 from cv2.data import haarcascades as haar_path
 
-from smile_detector.database_manager import DatabaseManager
 from smile_detector.app_config import config, logger
 logger.debug(f"PID={config.pid};Loading session manager")
 
+from smile_detector.database_manager import DatabaseManager
 DatabaseManager.initialize_database()
 
 class SessionManager:
     """Class to manage sessions and their metadata."""
     BASE_FPS_PER_REQUEST = 30
     # Load Haar cascade for smile detection
-    smile_cascade = cv2.CascadeClassifier(haar_path + "haarcascade_smile.xml")
+    SMILE_CLASSIFIER = cv2.CascadeClassifier(haar_path + "haarcascade_smile.xml")
+    webcam_idx: int = config.webcam_index
 
     def __init__(self, session_id: int | None = None):
         self.session_id: int | None = session_id
+        self.active: bool = False
+        self.timestamp: float | None = None
+        self.request_counter: int = 0
+
+    def _setup_session(self) -> None:
         if self.session_id is None:
             self.session_id = DatabaseManager.create_new_session()
-            logger.info(f"Session {self.session_id} started, session count: {DatabaseManager.get_active_session_count()}")
+            logger.info(f"PID={config.pid}; Session {self.session_id} started, active session count: {DatabaseManager.get_active_session_count()}")
 
         assert self.session_id is not None, "Error in creating session in DB"
-
         session_metadata = DatabaseManager.get_session(self.session_id)
-
         if session_metadata is None:
             raise ValueError(f"Session {self.session_id} not found in the database.")
 
         self.timestamp = session_metadata["timestamp"]
-        self.active: bool = session_metadata["active"]
+        self.active = session_metadata["active"]
 
 
     def close(self):
         """Close the session and update the database."""
-        if self.session_id is not None:
+        if self.session_id is None:
+            logger.warning("pid=${config.pid}; attempting to close session object without a session ID")
+        else:
             DatabaseManager.deactivate_session(self.session_id)
             logger.info(f"Session {self.session_id} ended.")
-            self.session_id = None
+        self.session_id = None
+        self.active = False
+        self.timestamp = None
 
-    def detect_smile(self, frame: MatLike) -> list:
+    @staticmethod
+    def _detect_smile(frame: MatLike) -> list:
         """Detect a smile in the given frame and return the coordinates of the rectangle."""
         gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert to grayscale
-        smiles = self.smile_cascade.detectMultiScale(
+        smiles = SessionManager.SMILE_CLASSIFIER.detectMultiScale(
             gray_frame,
             scaleFactor=1.5,
             minNeighbors=35,
@@ -56,21 +65,38 @@ class SessionManager:
         best_smile = max(smiles, key=lambda s: s[2] * s[3])
         return [best_smile]
 
-    def produce_frames(self: Self):
+    def _check_liveliness(self) -> bool:
+        # check if the session is still active
+        self.request_counter += 1
+        if self.request_counter % 4 == 0:
+            assert self.session_id is not None, "Error: attempting to check liveliness for a closed session"
+            session_metadata = DatabaseManager.get_session(self.session_id)
+            if session_metadata is None or not session_metadata["active"]:
+                logger.info(f"PID={config.pid}; Liveliness check failed, Session {self.session_id} was closed.")
+                self.close()
+                return False
+        return True
+
+    def _produce_frames(self: Self):
         """Producer logic that reads frames from the camera and writes to the database."""
-        cap: cv2.VideoCapture = cv2.VideoCapture(0)
+        cap: cv2.VideoCapture = cv2.VideoCapture(self.webcam_idx)
         while True:
             start = time.time()
-            res : Tuple[bool, MatLike | None] = cap.read()
-            success: bool = res[0]
-            if not success or res[1] is None:
-                cap.release()
-                self.close()
-                raise IOError("Failed to read from camera")
-            frame: MatLike = res[1]
 
+            # check the database to make sure the client didnt close the session
+            if self._check_liveliness() is False:
+                cap.release()
+                # check_liveliness will call self.close if the db session is not active
+                return
+
+            frame_result : Tuple[bool, MatLike | None] = cap.read()
+            if frame_result[0] is False or frame_result[1] is None:
+                cap.release()
+                raise IOError("Failed to read from camera")
+
+            frame: MatLike = frame_result[1]
             # Detect smiles in the frame
-            smiles = self.detect_smile(frame)
+            smiles = self._detect_smile(frame)
 
             coords = []
             # Draw rectangles around detected smiles
@@ -86,7 +112,7 @@ class SessionManager:
                 )
             # Encode the frame as JPEG
             _, buffer = cv2.imencode('.jpg', frame)
-            frame_bytes = buffer.tobytes()
+            frame_bytes: bytes = buffer.tobytes()
 
             # Write the frame to the database
             assert self.session_id is not None, "Error: attempting to produce frames for a closed session"
@@ -97,6 +123,7 @@ class SessionManager:
             # Limit the frame rate to avoid contention on camera when we have concurrent sessions
             time.sleep(max(0.3 - (time.time() - start), 0))
 
+
     def get_latest_coords(self, session_id: int) -> dict:
         """Get the latest coordinates for the specified session."""
         if self.session_id is None:
@@ -104,16 +131,18 @@ class SessionManager:
 
         return DatabaseManager.get_latest_coords(session_id)
 
+
     def generate_frame_responses(self):
         """Generate responses for the frames."""
-        genrtr = self.produce_frames()
+        genrtr = self._produce_frames()
         while True:
             try:
                 frame = next(genrtr)
+                assert frame
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
             except Exception as e:
                 logger.error(f"PID={config.pid};Producer failed, stopping frame generation;{traceback.format_exc()}")
                 self.close()
                 raise
-            if frame:
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
